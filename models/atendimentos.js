8965fc2e34529242ca479559575c2a5063dd4b66
Marcelo Camillo <marcelocamillo18@gmail.com>
2021-07-19 22:55:13 -0300

    Aplicando validações de entrada

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -7,16 +7,40 @@ class Atendimento {
     const data = moment(atendimento.data, 'DD/MM/YYYY').format(
       'YYYY-MM-DD HH:MM:SS'
     );
-    const atendimentoDatado = { ...atendimento, dataCriacao, data };
-    const sql = 'INSERT INTO atendimentos SET ?';
 
-    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-      if (erro) {
-        res.status(400).json(erro);
-      } else {
-        res.status(201).json(resultados);
-      }
-    });
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteEhValido = atendimento.cliente.length >= 5;
+
+    const validacoes = [
+      {
+        nome: 'data',
+        valido: dataEhValida,
+        mensagem: 'Data deve ser maior ou igual a data atual.',
+      },
+      {
+        nome: 'cliente',
+        valido: clienteEhValido,
+        mensagem: 'O nome do cliente deve ter pelo menos 5 caracteres.',
+      },
+    ];
+
+    const erros = validacoes.filter((campo) => !campo.valido);
+    const existemErros = erros.length;
+
+    if (existemErros) {
+      res.status(400).json(erros);
+    } else {
+      const atendimentoDatado = { ...atendimento, dataCriacao, data };
+      const sql = 'INSERT INTO atendimentos SET ?';
+
+      conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+        if (erro) {
+          res.status(400).json(erro);
+        } else {
+          res.status(201).json(resultados);
+        }
+      });
+    }
   }
 }
 
