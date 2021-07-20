f2f569e5c1cb65c37460e684aae71a8592478c3e
Marcelo Camillo <marcelocamillo18@gmail.com>
2021-07-19 22:45:24 -0300

    Status HTTP

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -8,8 +8,6 @@ module.exports = (app) => {
   app.post('/atendimentos', (req, res) => {
     const atendimento = req.body;
 
-    Atendimento.adiciona(atendimento);
-
-    res.send('Você está na rota de atendimentos e está realizando um POST.');
+    Atendimento.adiciona(atendimento, res);
   });
 };
