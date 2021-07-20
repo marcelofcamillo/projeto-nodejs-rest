157a3589ea2229c2b28479d6162bca9883eb517a
Marcelo Camillo <marcelocamillo18@gmail.com>
2021-07-19 21:52:49 -0300

    Base de dados com MySQL

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const customExpress = require('./config/customExpress');
-const app = customExpress();
-const porta = 3000;
 
-app.listen(porta, () => {
-  console.log(`Servidor rodando na porta ${porta}.`);
+const conexao = require('./infraestrutura/conexao');
+conexao.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    const app = customExpress();
+    const porta = 3000;
+
+    app.listen(porta, () => {
+      console.log(`Servidor rodando na porta ${porta}.`);
+    });
+  }
 });
