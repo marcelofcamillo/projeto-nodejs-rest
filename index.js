56536942573a7b24cf429dac99d6656ff61c3c61
Marcelo Camillo <marcelocamillo18@gmail.com>
2021-07-19 22:03:13 -0300

    Tabela de atendimentos

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const customExpress = require('./config/customExpress');
-
+const Tabelas = require('./infraestrutura/tabelas');
 const conexao = require('./infraestrutura/conexao');
 conexao.connect((erro) => {
   if (erro) {
     console.log(erro);
   } else {
+    console.log('Conectado com sucesso!');
+
+    Tabelas.init(conexao);
+
     const app = customExpress();
     const porta = 3000;
 
---
+++ infraestrutura/tabelas.js
@@ -0,0 +1,30 @@
+class Tabelas {
+  init(conexao) {
+    this.conexao = conexao;
+
+    this.criarAtendimentos();
+  }
+
+  criarAtendimentos() {
+    const sql =
+      'CREATE TABLE atendimentos (' +
+      'id INT NOT NULL AUTO_INCREMENT, ' +
+      'cliente VARCHAR(50) NOT NULL, ' +
+      'pet VARCHAR(20), ' +
+      'servico VARCHAR(20) NOT NULL, ' +
+      'status VARCHAR(20) NOT NULL, ' +
+      'observacoes TEXT, ' +
+      'PRIMARY KEY(id)' +
+      ')';
+
+    this.conexao.query(sql, (erro) => {
+      if (erro) {
+        console.log(erro);
+      } else {
+        console.log('Tabela atendimentos criada com sucesso!');
+      }
+    });
+  }
+}
+
+module.exports = new Tabelas();
